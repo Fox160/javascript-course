105ab87a4dbb4847a5717d90e2038eecf579ab8e
Ksenia Kostenko <ksenija-kstenk@mail.ru>
2019-08-04 18:57:51 +0400

    Add card movements

--- barley-break/script.js
+++ barley-break/script.js
@@ -1,27 +1,82 @@
+var container = '';
+
+createCards();
 shuffle();
 
+let emptyDiv = document.createElement('div');
+emptyDiv.classList.add('emptyCard');
+emptyDiv.setAttribute('row', 3);
+emptyDiv.setAttribute('column', 3);
+container.appendChild(emptyDiv);
+
+var button = document.createElement("button");
+    button.innerHTML = "Начать игру";
+    // button.onclick = shuffle;
+    document.body.appendChild(button);
+
+function moveCard() {
+    const columnAttr = this.getAttribute('column');
+    const rowAttr = this.getAttribute('row');
+
+    if (this.getAttribute('row') == emptyDiv.getAttribute('row')) {
+        if (+this.getAttribute('column') + 1 == emptyDiv.getAttribute('column')) {
+            //Right
+            emptyDiv.parentNode.insertBefore(emptyDiv, this);
+        } else if (+this.getAttribute('column') - 1 == emptyDiv.getAttribute('column')) {
+            //Left
+            emptyDiv.parentNode.insertBefore(this, emptyDiv);
+        } else {
+            return;
+        }
+    } else if (this.getAttribute('column') == emptyDiv.getAttribute('column')) {
+        const afterNode2 = emptyDiv.nextElementSibling;
+
+        if (
+            +this.getAttribute('row') + 1 == emptyDiv.getAttribute('row') ||
+            +this.getAttribute('row') - 1 == emptyDiv.getAttribute('row')
+        ) {
+            //Up or Down
+            this.replaceWith(emptyDiv);
+            emptyDiv.parentNode.insertBefore(this, afterNode2);
+        } else {
+            return;
+        }
+    } else {
+        return;
+    }
+    setNewAttribute(this, emptyDiv.getAttribute('column'), emptyDiv.getAttribute('row'));
+    setNewAttribute(emptyDiv, columnAttr, rowAttr);
+}
+
+function setNewAttribute(toNode, columnAttr, rowAttr) {
+    toNode.setAttribute('row', rowAttr);
+    toNode.setAttribute('column', columnAttr);
+}
+
 function createCards() {
-    let element = document.createElement('div');
-    element.classList.add('container');
-    document.body.appendChild(element);
+    container = document.createElement('div');
+    container.classList.add('container');
+    document.body.appendChild(container);
     for (let i = 0; i < 15; i++) {
         let innerDiv = document.createElement('div');
         innerDiv.classList.add('card');
         innerDiv.innerHTML += i + 1 + '';
-        element.appendChild(innerDiv);
+        container.appendChild(innerDiv);
+        innerDiv.addEventListener('click', moveCard);
     }
 }
 
 //В будущем использовать эту функцию при нажатии кнопки "Старт"
 function shuffle() {
-    createCards();
-    var container = document.getElementsByClassName('container')[0];
     var elementsArray = Array.prototype.slice.call(container.getElementsByClassName('card'));
     elementsArray.forEach(function(element) {
         container.removeChild(element);
     });
     shuffleArray(elementsArray);
-    elementsArray.forEach(function(element) {
+
+    elementsArray.forEach(function(element, i) {
+        element.setAttribute('row', parseInt(i / 4, 10));
+        element.setAttribute('column', parseInt(i % 4, 10));
         container.appendChild(element);
     });
 }
